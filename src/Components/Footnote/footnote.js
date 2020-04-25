2ad9426e2bf437e2b1184265128a9057c9439c28
David1456 <balaji.kka@gmail.com>
2020-04-25 15:49:05 +0530

    change footnoteyear

--- src/Components/Footnote/footnote.js
+++ src/Components/Footnote/footnote.js
@@ -2,7 +2,7 @@ import React from 'react'
 const Footer1=()=>{
     return(<div className="footer1">
         <div>
-        <p>Acme Web Deisgn, Copyright © 2017</p>
+        <p>Acme Web Deisgn, Copyright © 2020</p>
 
         </div>
      
